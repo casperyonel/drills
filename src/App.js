4b82bd896cdd40192ff3940a4ca204fd64b0b28b
Casper Yonel <yonelcasper@gmail.com>
2021-12-17 16:10:47 -0700

    1 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  
+  const [text, setText] = useState('')
+  
+  // function displayText(text) {
+  //   return text
+  // }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+
+      <input type="text" onChange={(e) => setText(e.target.value)}/>
+
+      {/* <input type="submit" onClick={displayText} /> */}
+
+       <div>{text}</div>
     </div>
   );
 }
