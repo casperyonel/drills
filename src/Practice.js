a787879021823087db16eed76996fb44864106ac
Casper Yonel <yonelcasper@gmail.com>
2021-12-21 13:13:06 -0700

    s

--- src/Practice.js
+++ src/Practice.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import Theme from './Theme';
-
-
+import { useState, useEffect } from 'react';
+import ReactPlaceholder from 'react-placeholder';
+ 
 const Practice = ({ title }) => {
 
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        async function delayFunc() {
+            try {
+                await setTimeout(1000000)
+                setIsLoading(false)
+            } catch {
+                setIsLoading(false)
+            }
+        }
+        delayFunc()
+    }, [])
+
+
+    if (isLoading === true) return <div>Loading...</div>
 
     return (
         <div>
-            <h2>{title}</h2>
+            <ReactPlaceholder type="media" rows={7} ready={isLoading === false}>
+                <h2>{title}</h2>
+            </ReactPlaceholder>
         </div>
     )
 }
