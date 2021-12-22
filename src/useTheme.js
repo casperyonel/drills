98a71be9249092b796eee4ce07d8ff30d44ae4e6
Casper Yonel <yonelcasper@gmail.com>
2021-12-22 10:20:03 -0700

    theme component added

---
+++ src/useTheme.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react'
+
+const useTheme = (startingTheme = 'light') => {    
+
+    const [theme, setTheme] = useState(startingTheme)
+
+    function validateTheme(themeValue) {
+        if (themeValue === 'dark') {
+            setTheme('dark')
+        } else {
+            setTheme('light')
+        }
+    }
+
+    return { // Returning an object, so need to destructure when importing component
+        theme,
+        setTheme: validateTheme, // This function makes it auto light theme
+    }
+}
+
+export default useTheme;
